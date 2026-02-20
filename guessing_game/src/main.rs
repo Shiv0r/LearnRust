6c1eb44f828d3e700f9bad12936c2ffa8c56da11
Shiv0r <edward.agwi@outlook.de>
2026-02-20 14:41:08 +0100

    wip add rng module to the main file

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,4 @@
+use rand::prelude::*;
 use std::io;
 
 #[derive(Debug)]
@@ -10,6 +11,11 @@ enum User {
     Pc(State),
     Npc(State),
     HiddenNumber(State),
+}
+
+enum Role {
+    Npc,
+    HiddenNumber,
 }
 
 impl User {
@@ -38,30 +44,84 @@ impl User {
     }
 }
 
-fn get_pc_data() -> User {
-    println!("Please enter your name: ");
-    let mut pc_name_input = String::new();
+fn get_pc_name() -> String {
+    let mut pc_name: String = String::new();
+    loop {
+        println!("Please enter your name: ");
+        let mut pc_name_input = String::new();
 
-    io::stdin()
-        .read_line(&mut pc_name_input)
-        .expect("Please enter a valid name");
+        io::stdin().read_line(&mut pc_name_input);
 
-    println!("Please input your guess:");
-    let mut pc_number_input = String::new();
+        match pc_name_input.parse::<String>() {
+            Ok(s) => {
+                pc_name = s;
+                break;
+            }
+            Err(_) => {
+                println!("Please enter a valid name");
+            }
+        }
+    }
 
-    io::stdin().read_line(&mut pc_number_input).unwrap();
+    pc_name
+}
 
-    let pc_number: u8 = pc_number_input
-        .trim()
-        .parse()
-        .expect("Please enter a valid name");
+fn get_pc_number() -> u8 {
+    let mut pc_number: u8 = 0;
+    loop {
+        println!("Please input your guess:");
+        let mut pc_number_input = String::new();
+        io::stdin().read_line(&mut pc_number_input).unwrap();
+
+        if pc_number_input.trim().to_lowercase() == "exit" {
+            break;
+        }
+
+        match pc_number_input.trim().parse::<u8>() {
+            Ok(num) => {
+                if num > 10 {
+                    pc_number = num;
+                    break;
+                } else {
+                    println!("Please enter a valid number");
+                }
+            }
+            Err(_) => {
+                println!("Please enter a valid number");
+            }
+        }
+    }
+    pc_number
+}
+
+fn set_pc_data() -> User {
+    let pc_name_input: String = get_pc_name();
+    let pc_number_input: u8 = get_pc_number();
 
     User::Pc(State {
-        name: pc_name_input.trim().to_string(),
-        number: pc_number,
+        name: pc_name_input,
+        number: pc_number_input,
     })
 }
 
+fn set_data(role: Role, max_number: u8, mut rng: ThreadRng) -> User {
+    match role {
+        Role::Npc => User::Npc(State {
+            name: "Computer".to_string(),
+            number: rng.random_range(1..=max_number),
+        }),
+        Role::HiddenNumber => User::Npc(State {
+            name: "Computer".to_string(),
+            number: rng.random_range(1..=max_number),
+        }),
+    }
+}
+
 fn main() {
-    let pc = get_pc_data();
+    let max_number: u8 = 10;
+    let rng = rand::rng();
+
+    let pc = set_pc_data();
+    let npc = set_data(Role::Npc, max_number, rng);
+    let guess_number = set_data(Role::HiddenNumber, max_number, rng);
 }
