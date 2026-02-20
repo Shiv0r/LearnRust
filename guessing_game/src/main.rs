7e3a9ef9c4d647983cbe42404df7d49f03f5065b
Shiv0r <edward.agwi@outlook.de>
2026-02-20 15:49:44 +0100

    finished guess game for rust

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,7 @@
 use rand::prelude::*;
-use std::io;
+use std::io::{self, Write};
+
+const MAX_NUMBER: u8 = 10;
 
 #[derive(Debug)]
 struct State {
@@ -7,69 +9,79 @@ struct State {
     number: u8,
 }
 
-enum User {
+enum UserData {
     Pc(State),
     Npc(State),
     HiddenNumber(State),
 }
 
 enum Role {
+    Pc,
     Npc,
     HiddenNumber,
 }
 
-impl User {
+impl UserData {
     fn process_winner(&self) {
         match self {
-            User::Npc(state) => println!("{} won with the number {}", state.name, state.number),
-            User::Pc(state) => println!("{}, you won with the number {}", state.name, state.number),
-            User::HiddenNumber(state) => println!("{} is the hidden number!", state.name),
+            UserData::Npc(state) => println!("{} won with the number {}", state.name, state.number),
+            UserData::Pc(state) => {
+                println!("{}, you won with the number {}", state.name, state.number)
+            }
+            UserData::HiddenNumber(state) => println!("{} is the hidden number!", state.number),
+        }
+    }
+    fn process_loser(&self) {
+        match self {
+            UserData::Pc(state) => {
+                println!("{}, you lost. with the number {}", state.name, state.number)
+            }
+            UserData::Npc(state) => {
+                println!("{} lost with the number {}", state.name, state.number)
+            }
+            UserData::HiddenNumber(_) => print!(""),
         }
     }
 
     fn get_number(&self) -> u8 {
         match self {
-            User::Npc(state) => state.number,
-            User::Pc(state) => state.number,
-            User::HiddenNumber(state) => state.number,
-        }
-    }
-
-    fn get_name(&self) -> String {
-        match self {
-            User::Npc(state) => state.name.to_string(),
-            User::Pc(state) => state.name.to_string(),
-            User::HiddenNumber(state) => state.name.to_string(),
+            UserData::Npc(state) => state.number,
+            UserData::Pc(state) => state.number,
+            UserData::HiddenNumber(state) => state.number,
         }
     }
 }
 
 fn get_pc_name() -> String {
-    let mut pc_name: String = String::new();
     loop {
-        println!("Please enter your name: ");
+        print!("Please enter your name: ");
+        io::stdout().flush().unwrap();
+
         let mut pc_name_input = String::new();
 
-        io::stdin().read_line(&mut pc_name_input);
+        io::stdin().read_line(&mut pc_name_input).unwrap();
 
         match pc_name_input.parse::<String>() {
-            Ok(s) => {
-                pc_name = s;
-                break;
+            Ok(input) => {
+                if !input.trim().is_empty() {
+                    return input.trim().to_string();
+                } else {
+                    println!("Please enter a valid name");
+                }
             }
             Err(_) => {
                 println!("Please enter a valid name");
             }
         }
     }
-
-    pc_name
 }
 
 fn get_pc_number() -> u8 {
     let mut pc_number: u8 = 0;
     loop {
-        println!("Please input your guess:");
+        print!("Please input your guess: ");
+        io::stdout().flush().unwrap();
+
         let mut pc_number_input = String::new();
         io::stdin().read_line(&mut pc_number_input).unwrap();
 
@@ -79,7 +91,7 @@ fn get_pc_number() -> u8 {
 
         match pc_number_input.trim().parse::<u8>() {
             Ok(num) => {
-                if num > 10 {
+                if num <= MAX_NUMBER {
                     pc_number = num;
                     break;
                 } else {
@@ -94,34 +106,52 @@ fn get_pc_number() -> u8 {
     pc_number
 }
 
-fn set_pc_data() -> User {
+fn set_pc_data() -> UserData {
     let pc_name_input: String = get_pc_name();
     let pc_number_input: u8 = get_pc_number();
 
-    User::Pc(State {
+    UserData::Pc(State {
         name: pc_name_input,
         number: pc_number_input,
     })
 }
 
-fn set_data(role: Role, max_number: u8, mut rng: ThreadRng) -> User {
+fn set_data(role: Role, mut rng: ThreadRng) -> UserData {
     match role {
-        Role::Npc => User::Npc(State {
+        Role::Pc => set_pc_data(),
+        Role::Npc => UserData::Npc(State {
             name: "Computer".to_string(),
-            number: rng.random_range(1..=max_number),
+            number: rng.random_range(1..=MAX_NUMBER),
         }),
-        Role::HiddenNumber => User::Npc(State {
-            name: "Computer".to_string(),
-            number: rng.random_range(1..=max_number),
+        Role::HiddenNumber => UserData::HiddenNumber(State {
+            name: "hidden number".to_string(),
+            number: rng.random_range(1..=MAX_NUMBER),
         }),
     }
 }
 
+fn select_winner(pc: UserData, npc: UserData, hidden_number: UserData) {
+    let dist_pc = u8::abs_diff(hidden_number.get_number(), pc.get_number());
+    let dist_npc = u8::abs_diff(hidden_number.get_number(), npc.get_number());
+
+    if dist_npc < dist_pc {
+        hidden_number.process_winner();
+        npc.process_winner();
+        pc.process_loser();
+    } else {
+        hidden_number.process_winner();
+        pc.process_winner();
+        npc.process_loser();
+    }
+}
+
 fn main() {
-    let max_number: u8 = 10;
     let rng = rand::rng();
 
-    let pc = set_pc_data();
-    let npc = set_data(Role::Npc, max_number, rng);
-    let guess_number = set_data(Role::HiddenNumber, max_number, rng);
+    println!("Guess Game!\nTry to guess the hidden number against the computer!");
+    let pc = set_data(Role::Pc, rng.clone());
+    let npc = set_data(Role::Npc, rng.clone());
+    let hidden_number = set_data(Role::HiddenNumber, rng.clone());
+
+    select_winner(pc, npc, hidden_number);
 }
